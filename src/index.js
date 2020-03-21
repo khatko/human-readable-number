922f0dfccf1986d9d4c88f966b45d9fa2b1ad2ca
Natalie <natali14441@gmail.com>
2020-03-21 17:04:26 +0300

    feat: add function numbers to readable text

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
+const NUMBERS = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+    20: "twenty",
+    30: "thirty",
+    40: "forty",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy",
+    80: "eighty",
+    90: "ninety",
+    100: "hundred",
+    1000: "thousand"
+};
+
 module.exports = function toReadable (number) {
-  
+    let numberStr = number.toString();  
+    let numberLength = numberStr.length; 
+    let x = Math.pow(10, numberLength - 1); 
+
+    if (number <= 20 || number % x == 0) {
+        if (number % x == 0) {
+            return x > 10 ? NUMBERS[number / x] + " " + NUMBERS[x] : NUMBERS[number];
+        } else {
+            return NUMBERS[number];
+        }
+    }
+
+    let result = ""; 
+    let first = Math.floor(number / x);
+    let second = number % x; 
+    
+    if (numberLength > 2) {
+        result += toReadable(first * x) + " " + toReadable(second);
+    } else if (numberLength == 2) {
+       	first *= x;
+        result += toReadable(first) + (second > 0 ? (" " + toReadable(second)) : "");
+    }
+
+    return result;
 }
